8c2ea8c24542c9f6e21465aa85552dbb5ed66bb7
RSokolov <roman-27@inbox.ru>
2023-12-07 15:12:12 +0300

    change

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-
+from django.conf import settings
+from django.core.paginator import Paginator
+import csv
 
 def index(request):
     return redirect(reverse('bus_stations'))
 
 
 def bus_stations(request):
+    db = settings.BUS_STATION_CSV
+    with open(db, 'r') as f:
+        file = csv.DictReader(f)
+
+
+        # print(num)
+        # for i in file:
+        #     print(i['ID'])
+
+        paginator = Paginator(num, 10)
+        page = paginator.get_page(5)
+
+
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
 
-    context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
-    }
-    return render(request, 'stations/index.html', context)
+        context = {
+            'bus_stations': file,
+            'page': page,
+        }
+        return render(request, 'stations/index.html', context)
