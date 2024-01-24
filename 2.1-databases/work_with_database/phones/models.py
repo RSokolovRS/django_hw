0e93870180058225ef742bf9ce1d7eee958a5424
RSokolovRS <roman-27@inbox.ru>
2024-01-24 22:24:49 +0300

    add change

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -3,4 +3,8 @@ from django.db import models
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    image = models.ImageField(upload_to='image')
+    release_date = models.DateField(auto_now_add=True)
+    lte_exists = models.BooleanField(default=True)
