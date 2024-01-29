1e91e69c0ebb9fe74dcecf3910c99a642b7ce20f
RSokolovRS <roman-27@inbox.ru>
2024-01-29 21:26:20 +0300

    add change

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -8,10 +8,13 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         pass
 
+
     def handle(self, *args, **options):
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
             # TODO: Добавьте сохранение модели
-            pass
+            p = Phone.objects.create(name=phone['name'], price=phone['price'], image=phone['image'],
+                                     release_date=phone['release_date'], lte_exists=phone['lte_exists'])
+            p.save()
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -6,5 +6,9 @@ class Phone(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     image = models.ImageField(upload_to='image')
-    release_date = models.DateField(auto_now_add=True)
+    release_date = models.DateField(auto_now_add=False)
     lte_exists = models.BooleanField(default=True)
+    # slug = models.SlugField(default=True)
+
+    def __str__(self):
+        return f'{self.name, self.price, self.image, self.release_date}'
