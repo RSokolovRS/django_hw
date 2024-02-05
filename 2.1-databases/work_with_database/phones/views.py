129a27425d6a65a8a3c418aab8d21f6bec5f5b33
RSokolov <roman-27@inbox.ru>
2024-02-05 14:55:24 +0300

    add

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from phones.models import Phone
+from django.db.models import Avg, Max, Min
+from django.db.models.functions import Lower
 
 def index(request):
     return redirect('catalog')
@@ -8,13 +10,14 @@ def index(request):
 def show_catalog(request):
     template = 'catalog.html'
     phones = Phone.objects.all()
-    context = {'phones': phones}
+    phones_name = phones.order_by(Lower('name'))
+    print(phones_name)
+    context = {'phones': phones_name}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
     phone = Phone.objects.get(slug=slug)
-    print(phone)
     context = {'phone': phone}
     return render(request, template, context)
