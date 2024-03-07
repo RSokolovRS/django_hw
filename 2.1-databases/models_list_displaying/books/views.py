d9d04799f59d651f1f59bcc7d2daf3f429e28555
RSokolov <roman-27@inbox.ru>
2024-03-07 15:53:16 +0300

    new change
    Signed-off-by: RSokolov <roman-27@inbox.ru>

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -12,24 +12,17 @@ def books_view(request):
     page_obj = paginator.get_page(page_number)
     context = {
         'books_list': page_obj,
-
         }
     return render(request, template, context)
 
-# def book_pub_date(request, pub_date):
-#     template = 'books/books_list.html'
-#     books = Book.objects.filter(pub_date=pub_date)
-#     context = {'books': books}
-#     return render(request, template, context)
 
 def view_page(request, pub_date=None):
     template = 'books/books_list.html'
     books = Book.objects.filter(pub_date=pub_date)
-    # paginator = Paginator(books, 1)
-    # page_number = request.GET.get('page')
-    # page_obj = paginator.get_page(pub_date)
+    books_all = Book.objects.all()
     context = {
         'books_list': books,
+        'books_all': books_all,
         }
 
     return render(request, template, context)
--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -27,6 +27,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', books_view, name='books'),
     path('books/<date:pub_date>/',  view_page, name='books'),
-    # path('books/<date:pub_date>/', view_page, name='books')
-
-]
+    ]
