19e1b15ca1921cbd8b91241fbc068be7d165a653
RSokolovRS <roman-27@inbox.ru>
2024-06-04 22:09:52 +0300

    Создание модели

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,20 @@
+import uuid
+
 from django.db import models
 
+
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+
+class Measurement(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    temperature = models.FloatField(verbose_name='Температура')
+    date_time = models.DateTimeField(verbose_name='Дата и Время')
+
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Место расположения')
+    description = models.CharField(max_length=255, verbose_name='Описание датчика')
+
+    def __str__(self):
+        return self.name
