17ceab7856e3590e16b96c3d9a3e4ba316f63bc7
RSokolovRS <roman-27@inbox.ru>
2024-06-05 15:57:31 +0300

    добавил изменения

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
 class Sensor(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Место расположения')
-    description = models.CharField(max_length=255, verbose_name='Описание датчика')
+    name = models.CharField(max_length=50, verbose_name='Название Датчика')
+    description = models.CharField(max_length=255, verbose_name='Место расположения')
 
     def __str__(self):
         return self.name
@@ -14,9 +14,9 @@ class Sensor(models.Model):
 
 class Measurement(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    created_at = models.FloatField(verbose_name='Температура')
-    date_time = models.DateTimeField(verbose_name='Дата и Время')
-    name = models.OneToOneField(Sensor, on_delete=models.CASCADE, related_name='sensor')
+    temperature = models.FloatField(verbose_name='Температура')
+    created_at = models.DateTimeField(verbose_name='Дата и Время')
+    sensor = models.OneToOneField(Sensor, on_delete=models.CASCADE, related_name='sensor')
 
 
 
